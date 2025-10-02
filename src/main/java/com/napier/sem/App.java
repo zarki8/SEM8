5a890d95a96f3bf7deb2b54cfec45e0ca656b237
Igor Zarkiewicz <igorinho888@gmail.com>
2025-10-02 12:17:14 +0100

    Finally Working!

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -5,27 +5,28 @@ import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.MongoCollection;
 import org.bson.Document;
 
-public class App
-{
-    public static void main(String[] args)
-    {
+public class App {
+    public static void main(String[] args) {
 
         // Connect to MongoDB
-        MongoClient mongoClient = new MongoClient("mongo-dbserver");
-        // Get a database - will create when we use it
-        MongoDatabase database = mongoClient.getDatabase("mydb");
-        // Get a collection from the database
-        MongoCollection<Document> collection = database.getCollection("test");
-        // Create a document to store
-        Document doc = new Document("name", "Kevin Sim")
-                .append("class", "DevOps")
-                .append("year", "2024")
-                .append("result", new Document("CW", 95).append("EX", 85));
-        // Add document to collection
-        collection.insertOne(doc);
+        try (MongoClient mongoClient = new MongoClient("localhost", 27017)) {
 
-        // Check document in collection
-        Document myDoc = collection.find().first();
-        System.out.println(myDoc.toJson());
+
+            // Get a database - will create when we use it
+            MongoDatabase database = mongoClient.getDatabase("mydb");
+            // Get a collection from the database
+            MongoCollection<Document> collection = database.getCollection("test");
+            // Create a document to store
+            Document doc = new Document("name", "Kevin Sim")
+                    .append("class", "DevOps")
+                    .append("year", "2024")
+                    .append("result", new Document("CW", 95).append("EX", 85));
+            // Add document to collection
+            collection.insertOne(doc);
+
+            // Check document in collection
+            Document myDoc = collection.find().first();
+            System.out.println(myDoc.toJson());
+        }
     }
 }
